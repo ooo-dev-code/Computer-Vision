9ad41851f9ba3cad9dcdf3b19c98e2cffc2c686d
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2025-02-01 14:21:57 +0100

    Create face_detect.py

---
+++ Recognizer/Basic Recognizer/Haar/face_detect.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+
+img = cv.imread('OpenCV\Photos\img1.png')
+cv.imshow("Image", img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Gray", gray)
+
+haar = cv.CascadeClassifier('OpenCV\Face Recognition\haar_face.xml')
+
+faces_rect = haar.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+print(len(faces_rect))
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0, 0, 255), thickness=2)
+cv.imshow("Rect", img)
+    
+cv.waitKey(0)
