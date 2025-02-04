540e37c1e3a6ae932af079091aa7a4043ede0d59
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2025-02-04 13:24:40 +0100

    Update funny_eyes.py

--- Recognizer/Face Filter/funny_eyes.py
+++ Recognizer/Face Filter/funny_eyes.py
@@ -1,21 +1,19 @@
-
 import cv2
 
 # Load the pre-trained Haar Cascade classifier for eye detection
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-# Start the video capture from the default camera
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the frame
+    # Detect eyes in the frame
     eyes = face_cascade.detectMultiScale(gray, scaleFactor=3.1, minNeighbors=5, minSize=(30, 30))
+    
     # Draw circles at the center of the eyes
     for (x, y, w, h) in eyes:
         center_x = x + w // 2
@@ -27,13 +25,10 @@ while True:
         cv2.circle(frame, (center_x, center_y), radius-20, (0, 0, 0), -1)
         cv2.circle(frame, (center_x, center_y), radius-25, (0, 0, 0), -1)
 
-    # Display the resulting frame
-    cv2.imshow('Face Detection', frame)
+    cv2.imshow('Eye Filter', frame)
 
-    # Break the loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the capture and close the windows
 cap.release()
 cv2.destroyAllWindows()
